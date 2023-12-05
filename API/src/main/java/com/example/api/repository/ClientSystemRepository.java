31453e15921cce3e580823547a65ffa41b37c72f
mateuscosta27 <mateuscosta27@gmail.com>
2023-12-04 22:46:59 -0300

    Aicionando o repositorio da aplicação

---
+++ API/src/main/java/com/example/api/repository/ClientSystemRepository.java
@@ -0,0 +1,7 @@
+package com.example.api.repository;
+
+import com.example.api.entity.ClientSystemEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientSystemRepository extends JpaRepository<ClientSystemEntity, Long> {
+}
